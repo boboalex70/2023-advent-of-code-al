266289fa108b192cb9892c8134cdb620ce0d5014
Alex Bochenek <alex.bochenek@tangramflex.com>
2023-12-01 10:29:04 -0500

    part 1 day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,33 @@
 fun main() {
+
+    fun calibrateCalibrationValue(input: String): Int {
+        var firstDigit = 0
+        var lastDigit = 0
+        var firstDigitFound = false
+
+        for (c in input) {
+            if (c.isDigit() && !firstDigitFound){
+                firstDigit = c.digitToInt()
+                firstDigitFound = true
+            }
+            if (c.isDigit()) lastDigit = c.digitToInt()
+        }
+        return (firstDigit * 10) + lastDigit
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { calibrateCalibrationValue(it) }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+
+
+//    // test if implementation meets criteria from the description, like:
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
